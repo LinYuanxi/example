99b4a44ad7b99128bfb4d8a4107e343003478e25
Lin Yuanxi <605972569@qq.com>
2021-11-24 04:17:37 +0800

    Use proper grammer.

--- main.c
+++ main.c
@@ -8,6 +8,6 @@ char name[255];
  fgets(name, 255, stdin);
  name[strlen(name)-1] = '\0'; /* remove the newline at the end */
 
- printf("Hello %s!\n", name);
+ printf("Hello, %s!\n", name);
  return 0;
 }
